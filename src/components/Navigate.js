eda3f286712df5e902f417cdb238d433e206b32a
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-09-17 10:46:36 +0100

    making the nagivation bar more mobile friendly

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -3,14 +3,21 @@ import {Link} from 'react-router-dom'
 
 function Navigate() {
     return (
-        <nav>
-            <ul className="nav nav-pills justify-content-end flex-column flex-md-row">
+        <nav class="navbar navbar-dark bg-primary navbar-expand-sm mb-5 fixed-top" id="navbar-site" >
+        <div className="container" >
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navigation"       aria-controls="navigation" aria-expanded="false" aria-label="Toggle Navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navigation"  >
+            <ul className="navbar-nav">
             <Link className="nav-item nav-link" to='/' > <li>Home</li></Link>
             <Link  className="nav-item nav-link"to='/' ><li>Administration</li></Link>
             <Link  className="nav-item nav-link"to='/' ><li>Admission</li></Link>
             <Link  className="nav-item nav-link"to='/Login'><li>Student's Login Portal</li></Link>
             <Link  className="nav-item nav-link"to='/Register'><li>Course Registration</li></Link>
             </ul>
+            </div>
+            </div>
         </nav>
     )
 }
