946cf8e93fc228b045dee59d5688ea76135314dc
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-09-17 21:56:20 +0100

    making the navigation bar be responsive but still have issue with the mobile view

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -3,23 +3,26 @@ import {Link} from 'react-router-dom'
 
 function Navigate() {
     return (
-        <nav class="navbar navbar-dark bg-primary navbar-expand-sm mb-5 fixed-top" id="navbar-site" >
-        <div className="container" >
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navigation"       aria-controls="navigation" aria-expanded="false" aria-label="Toggle Navigation">
-              <span class="navbar-toggler-icon"></span>
+        < >
+        <nav className="navbar navbar-dark bg-primary navbar-expand-sm">
+        <div className="container">
+        <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navigation"  aria-controls="navigation" aria-expanded="false" aria-label="Toggle Navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navigation"  >
+            <div className="collapse navbar-collapse" id="navigation">
             <ul className="navbar-nav">
-            <Link to='/home' > <li className="nav-item nav-link">Home</li></Link>
-            <Link to='/administration' ><li className="nav-item nav-link" >Administration</li></Link>
-            <Link to='/admission' ><li className="nav-item nav-link" >Admission</li></Link>
-            <Link to='/'><li className="nav-item nav-link" >Student's Login Portal</li></Link>
-            <Link to='/Register'><li className="nav-item nav-link" >Course Registration</li></Link>
+            <Link to='/home' className="nav-item nav-link"> <li >Home</li></Link>
+            <Link to='/administration'  className="nav-item nav-link"><li >Administration</li></Link>
+            <Link to='/admission' className="nav-item nav-link"><li >Admission</li></Link>
+            <Link to='/' className="nav-item nav-link active" ><li>Student's Login Portal</li></Link>
+            <Link to='/Register' className="nav-item nav-link"><li >Course Registration</li></Link>
             </ul>
             </div>
             </div>
         </nav>
-    )
+        
+        </>
+)
 }
 
 export default Navigate
