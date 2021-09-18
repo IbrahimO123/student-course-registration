211a5153455d3663b2ac388907e148861e45e465
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-09-18 05:43:44 +0100

    remove the switch util to see if NotFound page will render in all the pages and it does

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from './components/Login.js'
 import {BrowserRouter as Router,Switch, Route} from 'react-router-dom'
 import Register from './components/Register'
 import Navigate from './components/Navigate'
+import NotFoundPage from './components/NotFoundPage'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
      <Switch>
        <Route path="/" exact component={Login} />
        <Route path="/Register" component={Register} />
+       <Route component={NotFoundPage} />
      </Switch>
      </Router>
       
