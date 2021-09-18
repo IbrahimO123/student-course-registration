f97df92c0098399b0a34f60a8dda1c3afb570a87
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-09-18 05:40:52 +0100

    create a NotFound page so that any other route will display the text for better UI experience

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h4>404: Page not Found</h4>
+        </div>
+    )
+}
+
+export default NotFoundPage
