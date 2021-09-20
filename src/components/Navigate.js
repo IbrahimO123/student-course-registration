054c9b38440ad09197cf76e0cac410f10a629d79
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-09-20 23:25:36 +0100

    remove the overflow class i put on the navigation menu

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 
 function Navigate() {
     return (
-        <nav className="navbar navbar-dark bg-primary navbar-expand-sm fixed-top overflow-auto">
+        <nav className="navbar navbar-dark bg-primary navbar-expand-sm fixed-top">
         <div className="container">
         <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navigation"  aria-controls="navigation" aria-expanded="false" aria-label="Toggle Navigation">
             <span className="navbar-toggler-icon"></span>
